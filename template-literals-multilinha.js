7bbc732e36f51512ac74d04c842b3a03aaa9e4ed
Clayton1224 <claytonrichard153@gmail.com>
2024-05-02 17:18:52 -0400

    exemplo de template multilinhas

---
+++ template-literals-multilinha.js
@@ -0,0 +1,16 @@
+let textoMultilinha = `
+esta é uma string 
+que ocupa multiplas linhas
+e voce não precisa se preoucupar
+com quebras de linhas ou concatenação`;
+
+console.log(textoMultilinha);
+/*
+saida:
+*
+esta é uma string 
+que ocupa multiplas linhas
+e voce nao precisa se preocupar
+com quebras de linhas  ou concatenação
+*
+*/
