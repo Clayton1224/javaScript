e6fb1a3d69979772a1f2f8c3c91e312af705380b
Clayton1224 <claytonrichard153@gmail.com>
2024-05-02 17:06:50 -0400

    exemplo template literals

--- concatenacao-string.js
+++ concatenacao-string.js
@@ -1,4 +1,5 @@
 let nome = "Clayton";
 let idade = "17";
+//usando o template literals para cirar um string com variaveis
 let saudacao = "Ola meu nome é" + nome + "e eu tenho" + idade + " anos.";
 console,log(saudacao); //saida: "ola meu nome é Clayton e eu tenho 17 anos"
---
+++ template-literals.js
@@ -0,0 +1,6 @@
+let nome = "Clayton Richard";
+let idade = 17;
+
+// usando template literals para criar um string com variaveis
+let saudacao = `ola, meu nome é ${nome} e eu tenh o ${idade} anos`;
+console,log(saudacao); //saida: "ola meu nome é Clayton e eu tenho 17 anos"
