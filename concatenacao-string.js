cea3b41b86c9f5aaedcc77b0c96c325e787a0728
Clayton1224 <claytonrichard153@gmail.com>
2024-05-02 16:58:12 -0400

    exemplo de concatenacao string

---
+++ concatenacao-string.js
@@ -0,0 +1,4 @@
+let nome = "Clayton";
+let idade = "17";
+let saudacao = "Ola meu nome é" + nome + "e eu tenho" + idade + " anos.";
+console,log(saudacao); //saida: "ola meu nome é Clayton e eu tenho 17 anos"
