600e4c05b3bd6f65a931dfdea681e51bd81405a4
Clayton1224 <claytonrichard153@gmail.com>
2024-05-02 16:10:00 -0400

    exemplo tipo de dados

---
+++ tipos-dados.js
@@ -0,0 +1,14 @@
+let nome = "Clayton";  
+let verdadeiro = true;
+let falso = false
+let numero = 17;
+let reais = 13.50;
+let nulo = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = { 
+name: "Clayton",
+age: 17
+profession: "garçom e caixa"
+
+}
