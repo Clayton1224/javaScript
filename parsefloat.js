5bffdd9df81a399731a70c76780d3a3b06d6aac3
Clayton1224 <claytonrichard153@gmail.com>
2024-05-02 16:39:12 -0400

    exemplo parseFloat

---
+++ parsefloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.60";
+console.log(stringParaDecimal); //saida: 12.60
+console.log(typeof(stringParaDecimal)); //saida: sting 
+console.log(""); // saida:"12.60"
+//conversão da string "stringParaDecimal" para decimal
+let decimal = parseFloat(stringParaDecimal);
+console.log(Decimal); //saida: 12.60
+console.log(typeof(decimal)); //saida: number
