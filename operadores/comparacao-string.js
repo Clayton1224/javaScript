abe51325c11d7e426f3ff22c40d1828bf47de6c9
Clayton1224 <claytonrichard153@gmail.com>
2024-05-07 15:30:24 -0400

    atividade comparacao string

---
+++ operadores/comparacao-string.js
@@ -0,0 +1,8 @@
+let stringIguais  = "clayton" == "clayton"; //true 
+let stringDiferentes  = "clayton" == "clayton"; //false 
+let stringMenor  = "A" == "B"; //true 
+let stringMaior  = "C" == "D"; //false 
+console. log( Wanderson == Wanderson: ${stringsIguais} );
+console. log( wanderson == Wanderson: ${stringsDiferentes} );
+console. log( A < B: ${stringMenor} );
+console. log(C > D: ${stringMaior});
