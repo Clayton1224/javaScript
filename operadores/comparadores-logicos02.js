c26ba22df18c33db44c3528d176b139b2294c4a4
Clayton1224 <claytonrichard153@gmail.com>
2024-05-07 16:23:20 -0400

    comparadores logicos 02 e

---
+++ operadores/comparadores-logicos02.js
@@ -0,0 +1,21 @@
+let a = (1 <2) || (3 > 4)
+let b = (1 > 2) | | (3 > 4)
+let c = (10 < 20) && (20 < 30)
+let d = (20 < 30) && (30 > 40)
+
+// true
+// false
+// true
+// true
+let e = (20 < 30) && ! (30 > 40) // true
+console.log(`a, b, c, d, e`);
+
+I | false
+I| false
+&& true
+&& false = false
+&& !false = true
+
+= true
+= false
+= true
