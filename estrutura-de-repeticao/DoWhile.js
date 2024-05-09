7252fc91897776903e46a7e475964fd017371938
Clayton1224 <claytonrichard153@gmail.com>
2024-05-09 16:39:44 -0400

    atividade swicht case

--- estrutura-de-repeticao/DoWhile.js
+++ estrutura-de-repeticao/DoWhile.js
@@ -1,25 +1,9 @@
-// Função para exibir informações sobre cada classe de personagem
-function exibirInfoClasse(classe) {
-    switch (classe) {
-        case "Guerreiro":
-            console.log("Classe: arqueiros");
-            console.log("Descrição: Os arqueiros sao lutadores estrategistas e inteligentes lutam sempre com um plano.");
-            console.log("Habilidades: chuva de flechas, flecha venenosa.");
-            break;
-        case "Mago":
-            console.log("Classe: Bardo");
-            console.log("Descrição: Os bardos são animados e belos suportes suas musica podem curar os aliados e evoluir os poderes deles.");
-            console.log("Habilidades: musica curadora, musica evolutiva.");
-            break;
-        case "Ladino":
-            console.log("Classe: Lutador");
-            console.log("Descrição: Os lutadores sao capazes de realizar ataques fortes capazes de nocautear um inimigo em um ataque .");
-            console.log("Habilidades: ataque unico e, vida maxima.");
-            break;
-        default:
-            console.log("Classe não reconhecida. Escolha entre arqueiro, bardo ou Lutador.");
-    }
-}
+let numero = 1;
+let soma = 0;
+do {
 
-// Chamando a função e passando como parâmetro "Bardo"
-exibirInfoClasse("Bardo");
+soma += numero;
+numero++;
+} while (numero <= 5);
+console. log("A soma dos números de 1 a 5 é:", soma) ;
+// A soma dos números de 1 a 5 é: 15
