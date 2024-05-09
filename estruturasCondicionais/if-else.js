df75a04f162d4738fc2c3fb0670978eef62bf942
Clayton1224 <claytonrichard153@gmail.com>
2024-05-09 14:23:47 -0400

    atividade if else

--- estruturasCondicionais/if-else.js
+++
@@ -1,8 +0,0 @@
-let diaDaSemana = "quarta-feira";
-
-if (diaDaSemana === "sábado" | | diaDaSemana === "domingo") {
-console. log("É fim de semana!");
-} else {
-console. log("É dia de semana.");
-
-}
